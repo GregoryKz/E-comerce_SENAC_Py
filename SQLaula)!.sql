c5ad448ec5c933b1132cbbf1dd123a53af5e2cea
Gregory Klaus <gregory.klaus2015@gmail.com>
2025-08-05 21:58:45 -0300

    Add files via upload

---
+++ SQLaula)!.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE eCOMERCE;
+USE eCOMERCE;
+CREATE TABLE Produto(
+ID INT AUTO_INCREMENT PRIMARY KEY,
+Nome VARCHAR (255) NOT NULL,
+Desconto DECIMAL(5,2),
+Descricao TEXT,
+Valor DECIMAL (10,2) NOT NULL,
+Avalicao DECIMAL (2,1),
+Quantidade_Compra INT,
+Imagens TEXT,
+Material VARCHAR (100),
+Marca VARCHAR (100),
+Cor VARCHAR (50),
+Tamanho VARCHAR(50),
+Peso DECIMAL (10,2),
+Lote VARCHAR (50)
+);
+
+CREATE TABLE Celular (
+Produto_ID INT PRIMARY KEY,
+Chip VARCHAR (100),
+Memoria VARCHAR (100),
+Camera VARCHAR (100),
+Fone VARCHAR (100),
+Carregador VARCHAR (100),
+Sistema_Operacional VARCHAR (100),
+Polegadas DECIMAL (4,1),
+FOREIGN KEY (Produto_ID) REFERENCES Produto (ID) ON DELETE CASCADE
+);
+
+CREATE TABLE TV (
+Produto_ID INT PRIMARY KEY,
+Smart BOOLEAN,
+Imagem VARCHAR (255),
+Audio VARCHAR (255),
+Polegadas DECIMAL (4,1),
+FOREIGN KEY (Produto_ID) REFERENCES Produto (ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Tenis (
+Produto_ID INT PRIMARY KEY,
+Respiravel BOOLEAN,
+Sistema_Amortecimento VARCHAR (255),
+Cano VARCHAR (100),
+Cadarco VARCHAR (100),
+FOREIGN KEY (Produto_ID) REFERENCES Produto (ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Relogio(
+Produto_ID INT PRIMARY KEY,
+Pulseira VARCHAR (100),
+Sistema VARCHAR (100),
+Protecao VARCHAR (100),
+FOREIGN KEY (Produto_ID) REFERENCES Produto (ID) ON DELETE CASCADE
+);
